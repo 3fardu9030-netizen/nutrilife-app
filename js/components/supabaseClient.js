352e3c791bd64cf18dac8a956dd9506187d1933d
Fardeen <3.fardu9030@gmail.com>
2026-05-23 23:16:42 +0530

    Fixed Supabase authentication

--- js/components/supabaseClient.js
+++ js/components/supabaseClient.js
@@ -1,6 +1,6 @@
 // js/supabase.js
 
-const SUPABASE_URL = 'https://msabqzswtyujglgtjoum.supabase.co';
+const SUPABASE_URL = 'https://msapqzxsvtyujglpjoum.supabase.co';
 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zYXBxenhzdnR5dWpnbHBqb3VtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1MjU2NzgsImV4cCI6MjA5NTEwMTY3OH0.zyrHGssmoQqH5ZnlfFDVGxbAxGw9w3KBeKzvXkwnbfE';
 
